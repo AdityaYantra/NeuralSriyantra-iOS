b2ea861a81d1d92bcfa71c5e72c850015ea92d71
Aditya Acharya <aditiya.acharya910@gmail.com>
2025-08-19 13:13:01 -0500

    Initial Commit

---
+++ NeuralSriyantra/NeuralSriyantraApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuralSriyantraApp.swift
+//  NeuralSriyantra
+//
+//  Created by Aditya Acharya on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct NeuralSriyantraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
